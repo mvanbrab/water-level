aa17df8ab4d180b6b98ae0267971d41bb8a6cfce
Martin Vanbrabant <mvanbrab@users.noreply.github.com>
2020-06-22 20:18:12 +0200

    averaging the input for smoother display

--- software/WaterLevelReceiver/WaterLevelReceiver.ino
+++ software/WaterLevelReceiver/WaterLevelReceiver.ino
@@ -33,11 +33,19 @@ hd44780_I2Cexp lcd(LCD_ADDR);
 // The bar graph to display the percentage
 LCDbarGraph barGraph(lcd, 0, 0, 16, 0, 100);
 
+#define TRIGGER_INTERVAL 1000ul // milliseconds
 unsigned long t0;
 unsigned long t1;
-int i;
-#define l 256
-char buf[l];
+
+uint8_t iInputBuffer;
+#define BUFFER_LENGTH 256
+char inputBuffer[BUFFER_LENGTH];
+
+uint8_t iHistory;
+uint8_t nHistory;
+#define HISTORY_LENGTH 10
+int historyVol_l[HISTORY_LENGTH];
+double historyVol_percent[HISTORY_LENGTH];
 
 void setup() {
   Serial.begin(115200);
@@ -55,43 +63,50 @@ void setup() {
   lcd.print("l");
 
   t0 = millis();
-  i = 0;
+  iInputBuffer = 0;
+  iHistory = 0;
+  nHistory = 0;
 
 #if DEBUG == 1
   //consume some fake data; useful if not connected to the transmitter
-  strcpy(buf, "{\"t_C\": -15.00, \"distance_m\": 1.600, \"height_m\": 0, \"vol_l\": 0, \"vol_percent\": 0, \"low\": true}");
-  consume();
-  delay(5000);
-  strcpy(buf, "{ \"t_C\": 21.78, \"distance_m\": 0.790, \"height_m\": 1.340, \"vol_l\": 2680, \"vol_percent\": 89.35, \"low\": false}");
-  consume();
+  strcpy(inputBuffer, "{\"t_C\": -15.00, \"distance_m\": 1.600, \"height_m\": 0, \"vol_l\": 0, \"vol_percent\": 0, \"low\": true}");
+  for (uint8_t i = 0 ; i < HISTORY_LENGTH ; i++) {
+    consume();
+    delay(1000);
+  }
+  strcpy(inputBuffer, "{ \"t_C\": 21.78, \"distance_m\": 0.790, \"height_m\": 1.340, \"vol_l\": 2680, \"vol_percent\": 89.35, \"low\": false}");
+  for (uint8_t i = 0 ; i < HISTORY_LENGTH ; i++) {
+    consume();
+    delay(1000);
+  }
 #endif  
 }
 
 bool timeToTrigger() {
   t1 = millis();
-  return (t1 - t0 >= 1000ul);
+  return (t1 - t0 >= TRIGGER_INTERVAL);
 }
 
 void trigger() {
     Serial2.print("?");
     t0 = t1;
-    i = 0;
+    iInputBuffer = 0;
 }
 
 void collect() {
   if (Serial2.available() > 0) {
     char c;
     c = Serial2.read();
-    if (i == 0) {
+    if (iInputBuffer == 0) {
       if (c == '{') {
-        buf[i++] = c;
+        inputBuffer[iInputBuffer++] = c;
       }
-    } else if (i < l - 1) {
-      buf[i++] = c;
+    } else if (iInputBuffer < BUFFER_LENGTH - 1) {
+      inputBuffer[iInputBuffer++] = c;
       if (c == '}') {
-        buf[i] = '\0';
+        inputBuffer[iInputBuffer] = '\0';
         consume();
-        i = 0;
+        iInputBuffer = 0;
       }
     }
   }
@@ -100,10 +115,10 @@ void collect() {
 void consume() {
 #if DEBUG == 1
   Serial.println(t1);
-  Serial.println(buf);
+  Serial.println(inputBuffer);
 #endif
 
-  JSONVar myObject = JSON.parse(buf);
+  JSONVar myObject = JSON.parse(inputBuffer);
   if (JSON.typeof(myObject) != "object" ||
       !myObject.hasOwnProperty("vol_l") ||
       !myObject.hasOwnProperty("vol_percent") ||
@@ -113,9 +128,24 @@ void consume() {
 #endif
     return;
   }
+ 
+  historyVol_l[iHistory] = (int) myObject["vol_l"];
+  historyVol_percent[iHistory] = (double) myObject["vol_percent"];
+  iHistory = (iHistory + 1) % HISTORY_LENGTH;
+  if (nHistory < HISTORY_LENGTH) {
+    nHistory++;
+  }
+  long longVol_l = 0;
+  for (uint8_t i = 0 ; i < nHistory ; i++) {
+    longVol_l += historyVol_l[i];
+  }
+  int vol_l = longVol_l / nHistory;
+  double doubleVol_percent = 0;
+  for (uint8_t i = 0 ; i < nHistory ; i++) {
+    doubleVol_percent += historyVol_percent[i];
+  }
+  int vol_percent = doubleVol_percent / nHistory + 0.5;
   
-  int vol_l = (int) myObject["vol_l"];
-  int vol_percent = (double) myObject["vol_percent"] + 0.5;
   bool low = (bool) myObject["low"];
 
   publishOnSerial(vol_l, vol_percent, low);
